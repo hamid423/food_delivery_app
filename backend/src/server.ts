e250d09d8912ebe020358d6eb055cb6c7cf0fc05
hamid <hamid.jemaaoui@gmail.com>
2022-09-28 21:43:42 +0100

    connecting to mongodb and register-page

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,12 @@
+import dotenv from 'dotenv'
+dotenv.config()
+import { dbConnect } from './configs/database.config'
 import express from "express"
 import cors from "cors"
-import { sample_foods, sample_tags, sample_users } from "./data"
-import jwt from "jsonwebtoken"
+import foodRouter from './routers/food.router'
+import userRouter from './routers/user.router'
 
+dbConnect()
 
 const app = express()
 app.use(express.json())
@@ -12,54 +16,14 @@ app.use(cors({
     origin:["http://localhost:4200"]
 }))
 
-app.get("/api/foods", (req, res) => {
-  res.send(sample_foods)
-})
+app.use("/api/foods", foodRouter)
+app.use("/api/users", userRouter)
 
-app.get("/api/foods/search/:searchTerm", (req, res) => {
-  const searchTerm = req.params.searchTerm
-  const foods = sample_foods.filter( food => food.name.toLowerCase().includes( searchTerm.toLowerCase()))
-  res.send(foods)
 
-})
 
-app.get("/api/foods/tag", (req, res) => {
-  res.send(sample_tags)
-})
 
-app.get("/api/foods/tag/:tagName", (req, res) => {
-  const tagName = req.params.tagName
-  const foods = sample_foods.filter( food  => food.tags?.includes(tagName))
-  res.send(foods)
-})
-
-app.get("/api/foods/:id", (req, res) => {
-  const foodId = req.params.id
-  const food = sample_foods.find(food => food.id == foodId)
-  res.send(food)
-})
-
-app.post("/api/users/login", (req, res) => {
-  const {email, password } = req.body
-  const user = sample_users.find(user => user.email === email && user.password === password)
-
-  if(user){
-    res.send(generateTokenResponse(user))
-  }
-  else{
-    res.status(400).send("User name or password is not valid")
-  }
-})
  
-const generateTokenResponse = (user:any) => {
-  const token = jwt.sign({
-    email: user.email, isAdmin: user.isAdmin
-  }, 'super-secret', {
-    expiresIn:"30d"
-  })
-  user.token = token
-  return user
-}
+
 
 const port = 5000
 app.listen(port, () =>  {
