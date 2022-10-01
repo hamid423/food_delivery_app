8e43afe135715a78d38a546902a5027c59aebcd5
hamid <hamid.jemaaoui@gmail.com>
2022-10-01 14:49:51 +0100

    deploy to Heroku

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,6 +6,8 @@ import cors from "cors"
 import foodRouter from './routers/food.router'
 import userRouter from './routers/user.router'
 import orderRouter from './routers/order.router'
+import path from 'path';
+
 
 dbConnect()
 
@@ -25,9 +27,12 @@ app.use("/api/orders", orderRouter)
 
 
  
+app.use(express.static('public'));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname,'public', 'index.html'))
+})
 
-
-const port = 5000
+const port = process.env.PORT || 5000;
 app.listen(port, () =>  {
     console.log("website served on http://localohost:" + port)
 })
